b731d6d259f405a9eb5dbd597238d27adf5d1f39
sarath <93651605+Sarath-194@users.noreply.github.com>
2022-02-13 11:18:50 +0530

    Create skills.js

---
+++ scriptAndPages/skills.js
@@ -0,0 +1,48 @@
+let skills = document.getElementById("skills");
+skills.innerHTML = `
+<div class="max-width">
+<h2 class="title">SKILLS</h2>
+<div class="SkillsContent">
+
+    <div>
+        <i class="fab fa-html5"></i>
+        <p>HTML</p>
+    </div>
+    <div>
+        <i class="fab fa-css3-alt"></i>
+        <p>CSS</p>
+    </div>
+    <div>
+        <i class="fab fa-js-square"></i>
+        <p>Advanced JS</p>
+    </div>
+    <div>
+        <i class="fab fa-node-js"></i>
+        <p>ExpressJs</p>
+    </div>
+    <div>
+        <i class="fab fa-node-js"></i>
+        <p>NodeJs</p>
+    </div>
+    <div>
+        <i class="fas fa-database"></i>
+        <p>MongoDb</p>
+    </div>
+    <div>
+        <i class="fab fa-react"></i>
+        <p>React</p>
+    </div>
+    <div>
+        <i class="fab fa-react"></i>
+        <p>Redux</p>
+    </div>
+    <div>
+        <i class="fab fa-js"></i>
+        <p>EJS</p>
+    </div>
+
+
+    
+</div>
+</div>
+`;
