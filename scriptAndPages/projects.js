ed417dc2033b1a1ce7641ea6e69b480c2fbde12d
sarath <93651605+Sarath-194@users.noreply.github.com>
2022-02-13 11:18:42 +0530

    Create projects.js

---
+++ scriptAndPages/projects.js
@@ -0,0 +1,109 @@
+let projectsSection = document.getElementById("mainDivProjectsSection");
+projectsSection.innerHTML = `
+            <!-- vedix-clone 2.o -->
+            <div class="eachProjectDiv MailChimp">
+                <p class="ProjectName ">Vedix-Clone - 2.0</p>
+                <div class="ProjectContainer">
+                    <div class="ProjectImage">
+                        <img src="./images/vedix_2.0.png" alt=""/>
+                    </div>
+                    <div class="projectDescription">
+                    <p>Developed a clone of <a href="https://vedix.com/"
+                    style="color: #FFF; text-decoration: underline;" target="_blank">Vedix</a> website.<p >
+                        <br >
+                        Implimented maximum features of Vedix along with 2 Developers in 5 Days. We have made Project 
+                        into Chunks and Assigned particular part to one. Group meets were arranged through out project to plan the Day accordingly.  </p>
+                        <br>
+                        <u style="font-weight:bold;">TechStack :- </u>
+                        <br>
+                        <div class="techStackProject">
+
+                            <p><i class="fab fa-js-square"></i> EJS</p>
+                            <p><i class="fab fa-css3-alt"></i> CSS</p>
+                            <p> <i class="fab fa-js-square"></i> JS </p>
+                            <p> <i class="fab fa-node-js"></i> Node.JS </p>
+                            <p> <i class="fas fa-database"></i> MongoDB </p>
+                            <p> <i class="fab fa-node-js"></i> Express </p>
+                            
+                            
+                        </div>
+                        <div class="ProjectLinks">
+                        <a href="https://github.com/Sarath-194/vedix-clone-backend-express" target="_blank"><button>
+                        Github</button></a>
+                        <a href="https://vedix-clone-backend.herokuapp.com/" target="_blank"><button>Demo</button></a>
+
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <!-- vedix-clone -->
+            <div class="eachProjectDiv ">
+                <p class="ProjectName ">Vedix - Clone</p>
+                <div class="ProjectContainer">
+                    <div class="ProjectImage">
+                        <img src="./images/vedix_clone.png" alt="" />
+                    </div>
+                     <div class="projectDescription">
+                        <p>Developed a clone of <a href="https://vedix.com/"
+                                style="color: #FFF; text-decoration: underline;" target="_blank">Vedix</a> website.</p>
+                        <br >
+                        <p>it's Coloberative project, Built with 5 developers in 5 days. We have Implimented basic and required features of Vedix Website. Like, Sign-in/Sign-UP, Product page, Skin and Hair Quizzes for better filtration for Suitable issue.  </p>
+                        <br>
+                        <u style="font-weight:bold;">TechStack :- </u>
+                        <br>
+                        <div class="techStackProject">
+                            <p><i class="fab fa-html5"></i> HTML</p>
+                            <p><i class="fab fa-css3-alt"></i> CSS</p>
+                            <p> <i class="fab fa-js-square"></i> AdvancedJS </p>
+                            
+                        </div>
+                        <div class="ProjectLinks">
+                            <a href="https://github.com/Sarath-194/vedix-clone"
+                                target="_blank"><button>
+                                    Github</button></a>
+                            <a href="https://sarath-194.github.io/vedix-clone/home-page/vedix_body.html" target="_blank"><button>Demo</button></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- lybrate project -->
+            <div class="eachProjectDiv">
+                <p class="ProjectName">Walmart - Clone</p>
+                <div class="ProjectContainer">
+                    <div class="ProjectImage">
+                        <img src="./images/walmart_body.png" alt="" />
+                    </div>
+                    <div class="projectDescription">
+                        <p>Developed a clone of <a href="https://www.walmart.com/"
+                                style="color: #FFF; text-decoration: underline;" target="_blank">Walmart</a> website.</p>
+                        <br >
+                        <p>Walmart is an e-Commerce website. Along with 6 Developers project Walmart was initiated. 
+                        Build this clone in 5 days, We have implimented Sliding images, Product page, 
+                        Cart page, and Checkout page by Using Pure JavaScript.</p>
+                        </p>
+                        <br>
+                        <u style="font-weight:bold;">TechStack :- </u>
+                        <br>
+                        <div class="techStackProject">
+                            <p><i class="fab fa-html5"></i> HTML</p>
+                            <p><i class="fab fa-css3-alt"></i> CSS</p>
+                            <p> <i class="fab fa-js-square"></i> JS </p>
+                            
+                        </div>
+                        <div class="ProjectLinks">
+                            <a href="https://github.com/Sarath-194/walmart"
+                                target="_blank"><button>
+                                    Github</button></a>
+                            <a href="https://sarath-194.github.io/walmart/body.html" target="_blank"><button>Demo</button></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+`;
